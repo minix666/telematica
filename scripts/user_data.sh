588c518a22f1c91d4ba72a473a56a0ce8aac4f76
minix666 <manolo666colita@gmail.com>
2024-05-30 16:13:53 -0500

    Update user_data.sh

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -2,6 +2,6 @@
 
 sudo yum update -y
 sudo yum install git -y
-sudo git clone https://github.com/luu1003/telematica.git
+sudo git clone https://github.com/minix666/telematica.git
 sudo yum install -y docker
 sudo service docker start
